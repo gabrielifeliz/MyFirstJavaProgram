e7824f8ac2a000aa9bcfd213680f524ca3ac6b49
gabrielifeliz <gfeliz3@montgomerycollege.edu>
2018-05-31 14:59:15 -0400

    initial commit

---
+++ src/com/company/MyFirstJavaProgram.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is May 31, 2018. I am alive!");
+        System.out.println("Gabriel I. Feliz");
+    }
+}
